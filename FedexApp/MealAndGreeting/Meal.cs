910cbb04a3616f68afd465be995dfe9102e5a889
Jun Hashimoto <jun.hashimoto211@gmail.com>
2018-03-08 11:33:42 +1300

    Adding solution files

---
+++ FedexApp/MealAndGreeting/Meal.cs
@@ -0,0 +1,25 @@
+﻿namespace FedexApp.MealAndGreeting
+{
+    public class Meal
+    {
+        public string GetMeal(string time)
+        {
+            if (time?.ToLower() == "morning")
+            {
+                return "Breakfast";
+            }
+            else if (time?.ToLower() == "noon")
+            {
+                return "Lunch";
+            }
+            else if (time?.ToLower() == "evening")
+            {
+                return "Dinner";
+            }
+            else
+            {
+                return "Stay hugry!";
+            }
+        }
+    }
+}
